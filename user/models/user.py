3f69ce4553188fa223feaddfca71cd14f3518e69
Hieu <danhhieu123ns@gmail.com>
2025-03-16 10:34:22 +0700

    add cinema relationship to User model; update User schemas to include cinema_id and optional cinema field

--- user/models/user.py
+++ user/models/user.py
@@ -14,9 +14,11 @@ class User(Base):
     phone_number = Column(String, nullable=True)
     birthdate = Column(Date, nullable=True)
     address = Column(String, nullable=True)
-    
+    cinema_id = Column(Integer, ForeignKey("cinemas.id", ondelete="CASCADE"), nullable=True)
     is_active = Column(Boolean, default=True)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
 
     auth_credential = relationship("AuthCredential", back_populates="user", uselist=False, passive_deletes=True)
+    cinema = relationship("Cinema", back_populates="users", passive_deletes=True)
     user_roles = relationship("UserRole", back_populates="user", passive_deletes=True)
+    rates = relationship("Rate", back_populates="user", passive_deletes=True)
