aa8b44331292da589bc3cb4fd3897514c3224d55
Hieu <danhhieu123ns@gmail.com>
2025-04-05 02:03:38 +0700

    Refactor router imports in main.py: reorganize import statements for clarity and consistency

--- main.py
+++ main.py
@@ -7,19 +7,22 @@ from user_role.routers import user_role
 from auth_credential.routers import auth_credential
 from authen.routers import authen
 from cinema.routers import cinema
-from film.routers import film
-from food.routers import food
-from rate.routers import rate
 from room.routers import room
-from showtime.routers import showtime
-from promotion.routers import promotion
 from seat.routers import seat
-from ticket.routers import ticket
-from bill.routers import bill
-from bill_prom.routers import bill_prom
-from user_bill.routers import user_bill
+from film.routers import film
 from genre.routers import genre
 from film_genre.routers import film_genre
+from rate.routers import rate
+from showtime.routers import showtime
+from showtime_seat.routers import showtime_seat
+from ticket.routers import ticket
+from food.routers import food
+from bill.routers import bill
+from promotion.routers import promotion
+from bill_prom.routers import bill_prom
+from user_bill.routers import user_bill
+
+
 import uvicorn
 
 
@@ -47,19 +50,22 @@ app.router.include_router(user_role.router)
 app.router.include_router(auth_credential.router)
 app.router.include_router(authen.router)
 app.router.include_router(cinema.router)
-app.router.include_router(film.router)
-app.router.include_router(food.router)
-app.router.include_router(rate.router)
 app.router.include_router(room.router)
-app.router.include_router(showtime.router)
-app.router.include_router(promotion.router)
 app.router.include_router(seat.router)
-app.router.include_router(ticket.router)
-app.router.include_router(bill.router)
-app.router.include_router(bill_prom.router)
-app.router.include_router(user_bill.router)
+app.router.include_router(film.router)
 app.router.include_router(genre.router)
 app.router.include_router(film_genre.router)
+app.router.include_router(rate.router)
+app.router.include_router(showtime.router)
+app.router.include_router(showtime_seat.router)
+app.router.include_router(ticket.router)
+app.router.include_router(food.router)
+app.router.include_router(bill.router)
+app.router.include_router(promotion.router)
+app.router.include_router(bill_prom.router)
+app.router.include_router(user_bill.router)
+
+
 
 
 # if __name__ == "__main__":
